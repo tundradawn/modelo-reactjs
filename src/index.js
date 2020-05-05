2422fe23d807e2d4697a9e9fac1b9cd61ca6fa66
DanielTamkin <9532762+DanielTamkin@users.noreply.github.com>
2020-05-05 12:45:47 -0500

    Include GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import GlobalStyle from './styles/global';
+
 class App extends Component {
 
   render () {
     return (
       <React.Fragment>
+      <GlobalStyle />
         <h1>hello world</h1>
       </React.Fragment>
     );
